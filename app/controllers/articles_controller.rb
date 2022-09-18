b1b2d17c015f23e694b13d09d3a8363d41567e57
joaopedroazul <joaopedroandrade@aluno.unb.br>
2022-09-18 19:28:12 -0300

    Criando Controller Article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,68 @@
+class ArticlesController < ApplicationController
+    before_action :find_article, except: %i[ index ]
+
+
+    # GET /articles
+    def index
+        @article = Article.all
+        if @article.empty?
+            list = Article.new.get_news
+            list.each do |item| 
+                # puts "#{item[0]}\n\n#{item[1]}\n\n#{item[2]}\n\n#{item[3]}\n\n#{item[4]}"
+                title = item[0]
+                subTitle = item[1]
+                image = item[2]
+                caption = item[3]
+                paragraph = item[4]
+                puts "#{title}\n\n#{subTitle}\n\n#{image}\n\n#{caption}\n\n#{paragraph}"
+                @article = Article.new
+                @article.title = title
+                @article.subTitle = subTitle
+                @article.image = image
+                @article.caption = caption
+                @article.paragraph = paragraph
+                @article.save!
+            end 
+            @article = Article.all
+            render json: @article, status: :ok
+        else     
+            render json: @article, status: :ok
+        end    
+    end    
+    # POST /articles
+    def create
+         
+    end 
+    
+    # GET /articles/1
+    def show
+
+    end
+  
+  
+    # PATCH/PUT /articles/1
+    def update
+        if @article.update(article_params)
+            render json: @article
+        else
+            render json: @article.errors, status: :unprocessable_entity
+        end
+    end
+  
+    # DELETE /articles/1
+    def destroy
+        @article.destroy
+    end
+  
+    private
+    # Only allow a list of trusted parameters through.
+    def article_params
+        params.permit(:title, :subTitle, :image, :caption, :paragraph)
+    end
+
+    def find_article
+        @article = Article.find_by_id!(params[:id])
+    end
+       
+end
+
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,31 @@
 class Article < ApplicationRecord
+    require 'rss'
+    require 'open-uri' 
+    
+
+    def get_news
+        articles = []
+       
+    url = "https://g1.globo.com/rss/g1/brasil/"
+
+    URI.open(url) do |rss|
+        feed = RSS::Parser.parse(rss)
+
+        (0..9).each do |news|
+            dados = feed.items[news].description
+            img = dados.slice(0..dados.rindex("   ")).slice(dados.index("https")..dados.rindex(" /><br />")-2)
+            dadosBruto = dados.slice(dados.rindex("   ")..dados.size)
+            dadosBruto[dadosBruto.index("\n")] = " - "
+            text = dadosBruto.slice(dadosBruto.index(" - ")..dadosBruto.size)
+            subText = dadosBruto.slice(0..(dadosBruto.index(" - ") + text.index(/[a-z]\s[A-Z]/) )).to_s
+            subTitulo,legendaImg = subText.slice(0..subText.rindex('.')),subText.slice(subText.rindex('.')+1..subText.size)
+            texto = dadosBruto.slice((dadosBruto.index(" - ") + text.index(/[a-z]\s[A-Z]/) )+1..dadosBruto.size) 
+            textoFinal = texto.slice(0..texto.rindex('.'))
+            # puts feed.items[news].title.to_s, subTitulo, img, legendaImg, textoFinal
+            articles.push([feed.items[news].title.to_s, subTitulo, img, legendaImg, textoFinal])
+        end    
+
+    end
+        return articles
+    end 
 end
